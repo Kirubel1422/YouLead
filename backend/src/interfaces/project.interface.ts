5b2649bda6e0723587c12a4a1a7618088671df33
Kirubel1422 <kirubel739@gmail.com>
2025-03-26 21:54:26 -0400

    adds task and project interface

---
+++ backend/src/interfaces/project.interface.ts
@@ -0,0 +1,7 @@
+export interface ProjectStatus {
+  completed: number;
+  pending: number;
+  pastDue: number;
+  createdAt: string;
+  updatedAt: string;
+}
---
+++ backend/src/interfaces/task.interface.ts
@@ -0,0 +1,7 @@
+export interface TaskStatus {
+  completed: number;
+  pending: number;
+  pastDue: number;
+  createdAt: string;
+  updatedAt: string;
+}
