2241083fe7c9d9950bc23588cf357bd1231b1b7b
Kirubel1422 <kirubel739@gmail.com>
2025-05-14 20:18:10 -0400

    ✨ Adds messages type

---
+++ frontend/src/types/messages.types.ts
@@ -0,0 +1,43 @@
+import { IProfile } from "./user.types";
+
+export type SentInTypes = "dm" | "project" | "task";
+export type ReadMessageType = Partial<IProfile>;
+
+export interface IMessage {
+     id?: string;
+     sentBy: string;
+     readBy?: ReadMessageType[];
+     sentIn: SentInTypes;
+     receivedBy: string; // Represent's UID, Project ID, and Task ID
+     fileId?: string;
+     editted?: boolean;
+
+     msgContent: string;
+
+     createdAt?: string;
+     updatedAt?: string;
+}
+
+export type OnlineStatus = "online" | "offline";
+export type ChatType = "dm" | "project" | "task";
+
+export interface IChatUser {
+     id: string;
+     name: string;
+     avatar?: string;
+     initials: string;
+     status: OnlineStatus;
+     lastSeen?: string;
+     role?: string;
+}
+
+export interface IChat {
+     userId: string;
+     userData: IChatUser;
+     msgs: IMessage[];
+     // unreadCount: number;
+     msgCount: number;
+     lastMsg: Partial<IMessage>;
+     type: ChatType;
+     participants?: IChatUser[];
+}
