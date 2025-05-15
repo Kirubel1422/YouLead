0f5cb192ace2da53681fec54597f8ed6d72ef3f6
Kirubel1422 <kirubel739@gmail.com>
2025-05-14 20:17:04 -0400

    ✨ Adds messages api

---
+++ frontend/src/api/messages.api.ts
@@ -0,0 +1,32 @@
+import { DOTENV } from "@/constants/env";
+import { IChat } from "@/types/messages.types";
+import { IResponse } from "@/types/response.types";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const messagesApi = createApi({
+     reducerPath: "messagesApi",
+     baseQuery: fetchBaseQuery({
+          baseUrl: DOTENV.API_ENDPOINT + "/chat",
+          credentials: "include",
+     }),
+     tagTypes: ["Messages"],
+     endpoints: (builder) => ({
+          // Fetch messages
+          fetchDMMessages: builder.query<IChat[], void>({
+               query: () => ({
+                    url: `/messages`,
+               }),
+               transformResponse: (resp: IResponse) => (resp.success ? resp.data : []),
+               providesTags: (resp: IChat[] | undefined) =>
+                    Array.isArray(resp) && resp.length > 0
+                         ? [
+                                { type: "Messages", id: "LIST" },
+                                ...resp.map(({ userId }) => ({ type: "Messages" as const, id: userId })),
+                           ]
+                         : [],
+          }),
+     }),
+});
+
+export const { useFetchDMMessagesQuery } = messagesApi;
+export default messagesApi;
