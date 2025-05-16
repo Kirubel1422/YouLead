a7d409b3a270fea4ce7d459b8c63f4b40c0ed7fb
Kirubel1422 <kirubel739@gmail.com>
2025-05-16 14:29:53 -0400

    feat: task schema added

---
+++ frontend/src/schemas/task.schema.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+export type TaskStatusType = "pending" | "completed" | "pastDue";
+export type TaskPriorityType = "low" | "medium" | "high";
+
+export const TaskSchema = z.object({
+     name: z.string().min(1).max(100),
+     description: z.string().max(500).optional(),
+     status: z.enum(["pending", "completed", "pastDue"], {
+          message: "Status must be either pending, completed, or pastDue",
+     }),
+     progress: z.number().min(0).max(100),
+     priority: z.enum(["low", "medium", "high"], {
+          message: "Priority must be either low, medium, or high",
+     }),
+     assignedTo: z.array(z.string()).min(1, {
+          message: "Assigned to must have at least one user",
+     }),
+     deadline: z.array(z.string()).optional(),
+     projectId: z.string().min(1, {
+          message: "Project ID is required",
+     }),
+     teamId: z.string().min(1, {
+          message: "Team ID is required",
+     }),
+});
+
+export type TaskSchemaType = z.infer<typeof TaskSchema>;
