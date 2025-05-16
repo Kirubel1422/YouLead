2cfa1cd84e34dcc9df034c8700b68f9c5f351a05
Kirubel1422 <kirubel739@gmail.com>
2025-05-16 14:33:13 -0400

    updates components

--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -1,29 +1,42 @@
 import { cn } from "@/lib/utils";
 import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 export const AuthInput = ({
-  preIcon,
-  suffixIcon,
-  ...props
+     preIcon,
+     suffixIcon,
+     ...props
 }: React.InputHTMLAttributes<HTMLInputElement> & {
-  error?: string;
-  preIcon?: React.ReactNode;
-  suffixIcon?: React.ReactNode;
+     error?: string;
+     preIcon?: React.ReactNode;
+     suffixIcon?: React.ReactNode;
 }) => {
-  return (
-    <div>
-      <div className="relative">
-        {!!preIcon && preIcon}
+     return (
+          <div>
+               <div className="relative">
+                    {!!preIcon && preIcon}
 
-        <Input
-          {...props}
-          className={cn("pl-10 ", props.error && "border-red-500")}
-        />
+                    <Input {...props} className={cn("pl-10 ", props.error && "border-red-500")} />
 
-        {!!suffixIcon && suffixIcon}
-      </div>
+                    {!!suffixIcon && suffixIcon}
+               </div>
 
-      {props.error && <p className="text-xs text-red-500">{props.error}</p>}
-    </div>
-  );
+               {props.error && <p className="text-xs text-red-500">{props.error}</p>}
+          </div>
+     );
+};
+
+interface FormInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+     label: string;
+     error: string;
+}
+export const FormInput = ({ label, error, name, ...props }: FormInputProps) => {
+     return (
+          <div>
+               <Label htmlFor={name}>{label}</Label>
+               <Input {...props} className={cn("w-full", error && "border-red-500")} id={name} />
+
+               {error && <p className="text-sm text-red-500">{error}</p>}
+          </div>
+     );
 };
