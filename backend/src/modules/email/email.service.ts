014b761c80db5b05fecf60c5ceab5635531d39b7
Kirubel1422 <kirubel739@gmail.com>
2025-05-18 19:43:39 -0400

    feat: project assign/unassign email service

--- backend/src/modules/email/email.service.ts
+++ backend/src/modules/email/email.service.ts
@@ -1,4 +1,6 @@
 import { sendEmail } from "src/services/email";
+import { projectAssignTemplate } from "src/templates/projectAssign.template";
+import { projectUnAssignTemplate } from "src/templates/projectUnAssign.template";
 import { taskAssignTemplate } from "src/templates/taskAssign.template";
 import logger from "src/utils/logger/logger";
 
@@ -13,5 +15,25 @@ export class EmailService {
     const html = taskAssignTemplate(fullName, taskName);
     await sendEmail("Task Unassignment", to, html);
     logger.info("UnAssign email sent to: " + to);
+  }
+
+  static async projectAssign(
+    to: string,
+    projectName: string,
+    fullName: string
+  ) {
+    const html = projectAssignTemplate(fullName, projectName);
+    await sendEmail("Project Assignment", to, html);
+    logger.info("Project Assign email sent to: " + to);
+  }
+
+  static async projectUnAssign(
+    to: string,
+    projectName: string,
+    fullName: string
+  ) {
+    const html = projectUnAssignTemplate(fullName, projectName);
+    await sendEmail("Project Unassignment", to, html);
+    logger.info("Project UnAssign email sent to: " + to);
   }
 }
